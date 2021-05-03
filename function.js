8ba13fb12eb8d2e06c8334c64ec74d4d0d21c3e9
AlexMiller101 <AlexMiller101@users.noreply.github.com>
2021-05-03 13:46:42 +0530

    More Functionality

--- function.js
+++ function.js
@@ -1,11 +1,32 @@
-let content_area = document.getElementById('content-area');
 
-let saveBut = document.getElementById('saveBut');
-let openBut = document.getElementById('openBut');
-console.log("Hello, World");
-saveBut.addEventListener('click', saveStore);
 
-function saveStore(){
-    let content = content_area.innerHTML;
-    console.log('content is adding');
+// let saveBut = document.getElementById('saveBut');
+// let openBut = document.getElementById('openBut');
+// console.log("Hello, World");
+// saveBut.addEventListener('click', saveStore);
+
+// function saveStore(){
+//     let content = content_area.innerHTML;
+//     console.log('content is adding');
+// }
+
+window.onload = function() {
+    document.getElementById('saveBut').onclick = function() {
+        let value = document.getElementById('content-area').value;
+        
+        // storage code
+        chrome.storage.sync.set({"myData":value}, function(){
+            console.log('Success');
+        });
+
+    }
+    document.getElementById('openBut').onclick = function(){
+        // Storage retrieval Code
+        chrome.storage.sync.get('myData', function(data){
+            document.getElementById('content-area').value = data.myData;
+        });
+        
+    }
+     
 }
+
